94aa9afca7c70f805bac84eeb62772c1b512db87
alekseevayana <alekseevayana@bk.ru>
2021-06-29 15:31:44 -0700

    add media query to about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,17 +3,19 @@ import logo from "../img/profile-pic.png";
 
 export default function About() {
   return (
-    <div className="about">
-      <div className="about_left">
-        <h1 className="heading_1">Hi, I am Iana Alekseeva.</h1>
-        <p className="paragraph_1">
-          I am a Software Engineer with non-traditional background. I have a
-          passion for building websites, espesially from the front end side. I
-          am based in Silicon Valley.
-        </p>
-      </div>
-      <div>
-        <img src={logo} />
+    <div className="about_wrapper">
+      <div className="about">
+        <div className="about_left">
+          <h1 className="heading_1">Hi, I am Iana Alekseeva.</h1>
+          <p className="paragraph_1">
+            I am a Software Engineer with non-traditional background. I have a
+            passion for building websites, espesially from the front end side. I
+            am based in Silicon Valley.
+          </p>
+        </div>
+        <div>
+          <img src={logo} />
+        </div>
       </div>
     </div>
   );
